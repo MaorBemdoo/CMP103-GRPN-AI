188d63abc51d7b02fc9aabb7513b259b61424a9d
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-01 16:32:05 +0100

    Styled header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { GlobalStyles } from "./styles/GlobalStyles"
+import HeaderStyle from "./styles/Header.style"
 
 function App() {
 
   return (
     <>
       <GlobalStyles/>
+      <HeaderStyle/>
     </>
   )
 }
---
+++ src/styles/Header.style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import Header from "../components/Header/Header";
+
+const HeaderStyle = styled(Header)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export default HeaderStyle
