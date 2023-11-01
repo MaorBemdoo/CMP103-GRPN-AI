fadcb1283fb053e72aeb488b7921664d7c1088e4
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-01 17:22:22 +0100

    Worked on nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
 
 const Header = ({className}) => {
     return (
         <header className={className}>
             <a href='/'><img src=""/></a>
             <nav>
-                <a href="">Features</a>
-                <a href="">Premium</a>
-                <a href="">FAQ</a>
-                <a href="">Reviews</a>
+                <Typography variant="body1" component="a" href=''>Features</Typography>
+                <Typography variant="body1" component="a" href=''>Premium</Typography>
+                <Typography variant="body1" component="a" href=''>FAQ</Typography>
+                <Typography variant="body1" component="a" href=''>Reviews</Typography>
             </nav>
         </header>
     )
--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
     *{
-        color: #9e9bf7;
+        color: #7773fc;
         margin: 0;
         padding: 0;
         border: 0;
