5cf3ab6b6336b706855379dbfe5383195b1ccf8c
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-01 16:20:35 +0100

    Worked on markup of header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <a href='/'><img src=""/></a>
+            <nav>
+                <a href="">Features</a>
+                <a href="">Premium</a>
+                <a href="">FAQ</a>
+                <a href="">Reviews</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
